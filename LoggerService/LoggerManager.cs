cc3e6c775741bc179718969eeb278e2568a59e34
Tatsu sei <doubledragontaz@gmail.com>
2022-02-14 17:46:37 +0900

    first commit

---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Contracts;
+using NLog;
+using NLog.Fluent;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManger
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public LoggerManager()
+        {
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+    }
+}
